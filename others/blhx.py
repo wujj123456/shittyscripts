1367e687151e8b91b8d001697724b419df66de23
Junjie Wu <wujj@umich.edu>
2018-05-26 20:44:01 -0700

    blhx: refactor data tables

--- others/blhx.py
+++ others/blhx.py
@@ -10,40 +10,79 @@ from tabulate import tabulate
 from termcolor import colored, cprint
 
 
-ExpGrowth = namedtuple(
-    'ExpGrowth',
-    [
-        'base_lvl',
-        'end_lvl',
-        'step_base',
-        'step_growth',
-        'base_exp',
-    ],
-)
+# friendliness
 
-ExpGrowthTable = [
-    ExpGrowth(1, 40, 100, 100, 0),
-    ExpGrowth(40, 60, 4000, 200, 78000),
-    ExpGrowth(60, 70, 8000, 300, 196000),
-    ExpGrowth(70, 80, 11000, 400, 289500),
-    ExpGrowth(80, 90, 15000, 500, 417500),
-    ExpGrowth(90, 91, 0, 0, 590000),
-    ExpGrowth(91, 92, 0, 0, 610000),
-    ExpGrowth(92, 93, 0, 0, 631000),
-    ExpGrowth(93, 94, 0, 0, 653000),
-    ExpGrowth(94, 95, 0, 0, 677000),
-    ExpGrowth(95, 96, 0, 0, 703000),
-    ExpGrowth(96, 97, 0, 0, 733000),
-    ExpGrowth(97, 98, 0, 0, 768000),
-    ExpGrowth(98, 99, 0, 0, 808000),
-    ExpGrowth(99, 100, 0, 0, 868000),
-    ExpGrowth(100, 105, 70000, 2000, 1000000),
-    ExpGrowth(104, 106, 78000, 7000, 1292000),
-    ExpGrowth(106, 110, 97000, 12000, 1455000),
-    ExpGrowth(110, 115, 145000, 18000, 1915000),
-    ExpGrowth(115, 121, 235000, 21000, 2820000),
-]
+class ExpTable(object):
 
+    ExpGrowth = namedtuple(
+        'ExpGrowth',
+        [
+            'base_lvl',
+            'end_lvl',
+            'step_base',
+            'step_growth',
+            'base_exp',
+        ],
+    )
+
+    ExpGrowthTable = [
+        ExpGrowth(1, 40, 100, 100, 0),
+        ExpGrowth(40, 60, 4000, 200, 78000),
+        ExpGrowth(60, 70, 8000, 300, 196000),
+        ExpGrowth(70, 80, 11000, 400, 289500),
+        ExpGrowth(80, 90, 15000, 500, 417500),
+        ExpGrowth(90, 91, 0, 0, 590000),
+        ExpGrowth(91, 92, 0, 0, 610000),
+        ExpGrowth(92, 93, 0, 0, 631000),
+        ExpGrowth(93, 94, 0, 0, 653000),
+        ExpGrowth(94, 95, 0, 0, 677000),
+        ExpGrowth(95, 96, 0, 0, 703000),
+        ExpGrowth(96, 97, 0, 0, 733000),
+        ExpGrowth(97, 98, 0, 0, 768000),
+        ExpGrowth(98, 99, 0, 0, 808000),
+        ExpGrowth(99, 100, 0, 0, 868000),
+        ExpGrowth(100, 105, 70000, 2000, 1000000),
+        ExpGrowth(104, 106, 78000, 7000, 1292000),
+        ExpGrowth(106, 110, 97000, 12000, 1455000),
+        ExpGrowth(110, 115, 145000, 18000, 1915000),
+        ExpGrowth(115, 121, 235000, 21000, 2820000),
+    ]
+
+    def __init__(self):
+        self.table = {}
+        self._calc_table()
+
+    def _calc_table(self):
+        for e in self.ExpGrowthTable:
+            for lvl in range(e.base_lvl, e.end_lvl):
+                step = lvl - e.base_lvl
+                self.table[lvl] = (
+                    e.base_exp + step * e.step_base +
+                    sum(range(step)) * e.step_growth
+                )
+
+    def print_table(self, max_level=None):
+        rows = [
+            (k, v) for k, v in self.table.items()
+            if not max_level or k <= max_level
+        ]
+        print(tabulate(rows, headers=['level', 'total exp']))
+
+
+def print_friendiness(args):
+    exp = ExpTable()
+    if args.debug:
+        exp.print_table()
+
+    results = []
+    for lvl in range(args.start_level, args.end_level):
+        exp_diff = exp.table[args.end_level] - exp.table[lvl]
+        friendiness = exp_diff / args.exp_per_battle / 16
+        results.append((lvl, friendiness))
+    print(tabulate(results, headers=['level', 'friendiness gain']))
+
+
+# retire
 
 RETIRE_RESOURCE = {
     'DD': 15,
@@ -68,42 +107,6 @@ RETIRE_FUEL = {
     'BM': 3,
     'AR': 3,
 }
-
-
-class ExpTable(object):
-
-    def __init__(self, exp_growth_table):
-        self.table = {}
-        self._calc_table(exp_growth_table)
-
-    def _calc_table(self, exp_growth_table):
-        for e in exp_growth_table:
-            for lvl in range(e.base_lvl, e.end_lvl):
-                step = lvl - e.base_lvl
-                self.table[lvl] = (
-                    e.base_exp + step * e.step_base +
-                    sum(range(step)) * e.step_growth
-                )
-
-    def print_table(self, max_level=None):
-        rows = [
-            (k, v) for k, v in self.table.items()
-            if not max_level or k <= max_level
-        ]
-        print(tabulate(rows, headers=['level', 'total exp']))
-
-
-def print_friendiness(args):
-    exp = ExpTable(ExpGrowthTable)
-    if args.debug:
-        exp.print_table()
-
-    results = []
-    for lvl in range(args.start_level, args.end_level):
-        exp_diff = exp.table[args.end_level] - exp.table[lvl]
-        friendiness = exp_diff / args.exp_per_battle / 16
-        results.append((lvl, friendiness))
-    print(tabulate(results, headers=['level', 'friendiness gain']))
 
 
 def _color_table(table):
@@ -155,19 +158,19 @@ def print_retire_efficiency(headers, lvl_to_gain):
     for lvl, v in lvl_to_gain.items():
         rows.append([lvl] + v)
     print(tabulate(rows, headers=headers))
-    return
 
 
 def print_retire(args):
-    exp = ExpTable(ExpGrowthTable)
+    exp = ExpTable()
     print('==========\nEXP\n==========\n')
     exp.print_table(args.max_level)
 
-    print('\n==========\nRESOURCE\n==========\n')
+    multiplier = 100
+    print('\n==========\nRESOURCE (value, value per 100 xp)\n==========\n')
     print_retire_efficiency(
         *calc_retire_efficiency(exp, RETIRE_RESOURCE, 100, args.max_level),
     )
-    print('\n==========\nFUEL\n==========\n')
+    print('\n==========\nFUEL (value, value per 1000 xp)\n==========\n')
     print_retire_efficiency(
         *calc_retire_efficiency(exp, RETIRE_FUEL, 1000, args.max_level),
     )
